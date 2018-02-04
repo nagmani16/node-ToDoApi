cd7e4751334705395a92aeff55d32910891c96b1
nagmani16 <nagmani16@gmail.com>
2018-02-04 14:18:47 -0600

    added todo post code

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require ('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+
+var {Todo} = require('./model/Todo.js');
+
+var {User} = require ('./model/User.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res)=>{
+
+     var todo = new Todo({
+         text: req.body.text,
+     });
+
+     todo.save().then((doc) => {
+
+        res.send(doc);
+
+     }, (e) => {
+        res.status(400).send(e);
+     });
+});
+
+app.listen(3000, () => {
+
+    console.log('started on port 3000');
+});
