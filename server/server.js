dceb456704d209a0a5935f07fa52fda8a72fa3f1
nagmani16 <nagmani16@gmail.com>
2018-02-04 20:51:08 -0600

    added get route for Todos

--- server/server.js
+++ server/server.js
@@ -26,6 +26,16 @@ app.post('/todos', (req,res)=>{
      });
 });
 
+// Fetch Todos
+app.get('/todos',(req,res) => {
+
+    Todo.find().then((todos) =>{
+        res.send({todos})
+    }, (e) => {
+        res.status.send(e);
+    })
+})
+
 app.listen(3000, () => {
 
     console.log('started on port 3000');
